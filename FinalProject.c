9ef2c3238563daab4d323470e149826fbfbb7658
Andrew Rodriguez <andrewrb22@gmail.com>
2022-04-21 18:36:10 -0400

    Added structures of courses and students

--- FinalProject.c
+++ FinalProject.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+//-----------Courses Structure------------------//
+typedef struct course{
+
+    int CRN;
+    char course[10];
+    int credit_hr;
+} course;
+
+//--------------Student Structure-----------------//
+typedef struct student{
+int id; 
+char name[100];
+int course_cnt;
+
+
+}student;
+
+
+
+//-----------Main function-----------------------------------//
 
 int main(){
 
